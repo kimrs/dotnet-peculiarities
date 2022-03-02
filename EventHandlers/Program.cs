468e03de625f1a7ed0d8e19b0bc412203849b2c0
Kim Rune Solstad <krs@itverket.no>
2022-03-02 08:35:36 +0100

    Initial commit

---
+++ EventHandlers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace EventHandlers;
+
+public delegate void EventHandler(object? sender, EventArgs e);
+
+internal class FooCounter
+{
+    private int _total = 0;
+    public void Add()
+    {
+        _total++;
+        if (_total % 2 == 0)
+        {
+            OnFoo(EventArgs.Empty);
+        }
+    }
+
+    private void OnFoo(EventArgs e)
+    {
+        var handler = FooHappened;
+
+        handler(this, e);
+    }
+
+    public event EventHandler<EventArgs> FooHappened;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fooCounter = new FooCounter();
+        fooCounter.FooHappened += (x, y) => { Console.WriteLine("Foo happened"); };
+        var range = Enumerable.Range(0, 100);
+        foreach (var integer in range)
+        {
+            fooCounter.Add();
+        }
+    }
+}
